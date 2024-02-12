f11b293ec1536cb912fb90632fbe59db14158f80
root <root@penguin.friend.of.horse>
2024-02-12 20:08:42 +0100

    Now pipes are spawned at random height each time

--- Scene2.js
+++ Scene2.js
@@ -25,24 +25,30 @@ class Scene2 extends Phaser.Scene {
         this.pipes = this.physics.add.group();
         this.pipeWidth = 52; //softcode that
         this.pipeHeight = 320; //softcode that
+        this.baseHeight = 112;
         this.minGap = this.player.height * 4; //24 * 4 = 96;//440
-
+        this.normalGap = this.player.height * 6; //24 * 6 = 144
         //this.maxGap = max
         this.gap = this.minGap;
         this.minTopPipeHeight = 0 - (this.pipeHeight - 40); //-280
-        this.maxBottomPipeHeight = this.config.height; // 468
-        this.minBottomPipeHeight = this.maxBottomPipeHeight - this.pipeHeight + 112 + this.gap;
+        this.maxBottomPipeHeight = this.config.height - this.baseHeight; // 468
+        this.minBottomPipeHeight = this.maxBottomPipeHeight - this.pipeHeight + this.baseHeight + this.gap;
         this.maxTopPipeHeight = 0;
 
         this.heightTop = 0 - this.gap;
         this.heightBottom = this.heightTop + this.gap;
-        this.pipes.create(this.config.width, this.minTopPipeHeight, "pipe");
-        this.pipes.create(this.config.width, this.minBottomPipeHeight, "pipe"); //468
+        this.safeZone = this.config.height / 2 - this.baseHeight;
+        this.safeHeightTop = Phaser.Math.Between(this.minTopPipeHeight, this.maxTopPipeHeight);
+        this.safeHeightBottom = Phaser.Math.Between(this.minBottomPipeHeight, this.maxBottomPipeHeight);
+        this.pipes.create(this.config.width, this.safeHeightTop, "pipe");
+        this.pipes.create(this.config.width, this.safeHeightBottom, "pipe"); //468
         this.pipes.children.iterate(function (child) {
             child.body.allowGravity = false;
             child.setOrigin(0.5, 0); //bottom of unflipped sprite is origin
         });
         this.pipes.children.entries[0].setFlipY(true);
+
+        //this.safeZone - (this.gap / 2)
     }
 
     hit() {
@@ -59,11 +65,13 @@ class Scene2 extends Phaser.Scene {
     }
     ReplacePipePair() {
         if (this.pipes.children.entries[0].x < 0 - this.pipeWidth) {
+            this.safeHeightTop = Phaser.Math.Between(this.minTopPipeHeight, this.maxTopPipeHeight);
+            this.safeHeightBottom = Phaser.Math.Between(this.minBottomPipeHeight, this.maxBottomPipeHeight);
             this.pipes.children.entries[0].x = this.config.width;
-            this.pipes.children.entries[0].y = this.minTopPipeHeight;
+            this.pipes.children.entries[0].y = this.safeHeightTop;
 
             this.pipes.children.entries[1].x = this.config.width;
-            this.pipes.children.entries[1].y = this.minBottomPipeHeight;
+            this.pipes.children.entries[1].y = this.safeHeightBottom;
         }
     }
 
