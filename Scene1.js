3553a048aab954037a7f118c37811f8cb4daf407
root <root@penguin.friend.of.horse>
2024-02-12 16:18:27 +0100

    Basic physics and a pipe

--- Scene1.js
+++ Scene1.js
@@ -7,6 +7,7 @@ class Scene1 extends Phaser.Scene {
         this.load.image("background-day", "assets/sprites/background-day.png");
         this.load.image("yellowbird-midflap", "assets/sprites/yellowbird-midflap.png");
         this.load.image("base", "assets/sprites/base.png");
+        this.load.image("pipe", "assets/sprites/pipe-green.png");
     }
 
     create() {
--- Scene2.js
+++ Scene2.js
@@ -7,26 +7,39 @@ class Scene2 extends Phaser.Scene {
         this.background = this.add.image(0, 0, "background-day");
         this.background.setOrigin(0, 0);
 
-        var config = this.game.config;
+        this.config = this.game.config;
+        this.player = this.physics.add.sprite(this.config.width / 2, this.config.height / 2, "yellowbird-midflap");
+
         var platforms = this.physics.add.staticGroup();
-        this.player = this.physics.add.sprite(config.width / 2, config.height / 2, "yellowbird-midflap");
+        this.pipes = this.physics.add.group();
+        this.pipes.create(this.config.width, this.config.height, "pipe");
+        this.pipes.children.iterate(function (child) {
+            child.body.allowGravity = false;
+        });
+        platforms.create(this.config.width / 2, this.config.height, "base");
+
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
 
-        platforms.create(config.width / 2, config.height, "base");
         this.physics.add.collider(this.player, platforms);
+        this.physics.add.collider(this.player, this.pipes, this.hit, null, this);
         this.add.text(20, 20, "Playing game", { font: "25px Arial", fill: "yellow" });
+    }
+    hit() {
+        this.physics.pause();
     }
 
     update() {
         if (this.cursors.up.isDown) {
             this.player.setVelocityY(-250);
         }
+        this.pipes.setVelocityX(-100);
     }
     setupPlayer() {
         console.log("I am working");
     }
+
 }
